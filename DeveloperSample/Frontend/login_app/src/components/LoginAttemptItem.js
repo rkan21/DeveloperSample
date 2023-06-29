36e95e8219abae5452b9891e01e776c4f6385d51
Ryan K <ryan.kanzeg@protonmail.com>
2023-06-29 14:00:36 -0400

    cleaning up uncessary checkin cruft

--- DeveloperSample/Frontend/login_app/src/components/LoginAttemptItem.js
+++
@@ -1,9 +0,0 @@
-export const LoginAttemptItem = ({ loginAttempt }) => {
-  return (
-    <>
-      <li>
-          <span>{loginAttempt.username} - {loginAttempt.password}</span>
-      </li>
-    </>
-  )
-}
